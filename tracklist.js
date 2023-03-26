580205b971eb7ded8c50c28cf175cc7002e9af3c
Ben0096 <43043312+Ben0096@users.noreply.github.com>
2023-03-26 05:56:56 -0400

    Create tracklist.js

---
+++ tracklist.js
@@ -0,0 +1,26 @@
+let track_list = [
+    {
+        name: "Eustace",
+        artist: "David Keener",
+        image: "Eustace.png",
+        path: "Eustace.002.m4a"
+    },
+    {
+        name: "Good Day",
+        artist: "Twenty One Pilots",
+        image: "Scaled and Icy.png",
+        path: "Good Day.004.m4a"
+    },
+    {
+        name: "Grave",
+        artist: "Wage War",
+        image: "Grave.png",
+        path: "Grave.005.m4a"
+    },
+    {
+        name: "Highlights",
+        artist: "The Classic Crime",
+        image: "PITS.png",
+        path: "Highlights.006.m4a"
+    },
+];
