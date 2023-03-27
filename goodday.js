014f3f0762e71c338c1e4435c0a57b4a391f2ce7
Ben0096 <43043312+Ben0096@users.noreply.github.com>
2023-03-27 03:55:57 -0400

    Create goodday.js

---
+++ goodday.js
@@ -0,0 +1,14 @@
+let track_list = [
+    {
+        name: "Good Day v4",
+        artist: "Twenty One Pilots",
+        image: "Scaled%20and%20Icy.png",
+        path: "Good Day.004.m4a"
+    },
+    {
+        name: "Good Day v2",
+        artist: "Twenty One Pilots",
+        image: "Scaled%20and%20Icy.png",
+        path: "Good Day.002.m4a"
+    }
+];
