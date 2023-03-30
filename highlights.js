570c5e7a338b5da00a2dfca9cbf1106a5c12137e
Ben0096 <43043312+Ben0096@users.noreply.github.com>
2023-03-30 04:43:39 -0400

    Update highlights.js

--- highlights.js
+++ highlights.js
@@ -1,8 +1,8 @@
-let track_list = [
+track_list.push(
     {
         name: "Highlights",
         artist: "The Classic Crime",
         image: "PITS.png",
         path: "Highlights.007.m4a"
     }
-];
+);
