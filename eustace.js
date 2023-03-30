121912a5a838bb8cf24157aee53d2bde241c5344
Ben0096 <43043312+Ben0096@users.noreply.github.com>
2023-03-30 04:02:33 -0400

    Update eustace.js

--- eustace.js
+++ eustace.js
@@ -3,6 +3,6 @@ let track_list = [
         name: "Eustace",
         artist: "David Keener",
         image: "Eustace.png",
-        path: "Eustace.002.m4a"
+        path: "Eustace.003.m4a"
     }
 ];
