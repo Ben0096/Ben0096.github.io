e4a10ee75face644c7f2639cb00da87a1b7494c1
Ben0096 <43043312+Ben0096@users.noreply.github.com>
2023-03-30 04:42:32 -0400

    Update runninghome.js

--- runninghome.js
+++ runninghome.js
@@ -1,8 +1,8 @@
-let track_list = [
+track_list.push(
     {
         name: "Running Home",
         artist: "Cochren & Co.",
         image: "Running%20Home.png",
         path: "Running Home.002.m4a"
     }
-];
+);
