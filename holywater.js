333a8b466afa6b69ecfbd0788a5f758b2052796e
Ben0096 <43043312+Ben0096@users.noreply.github.com>
2023-04-02 06:56:35 -0400

    Update holywater.js

--- holywater.js
+++ holywater.js
@@ -2,7 +2,7 @@ track_list.push(
     {
         name: "Holy Water",
         artist: "We the Kingdom",
-        image: "Holy Water.png",
+        image: "Holy%20Water.png",
         path: "Holy%20Water.001.m4a"
     }
 );
