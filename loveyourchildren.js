6b6bed4e1bdc2f4e8b18e711ee356dad31cac05b
Ben0096 <43043312+Ben0096@users.noreply.github.com>
2023-06-07 23:07:59 -0400

    Create loveyourchildren.js

---
+++ loveyourchildren.js
@@ -0,0 +1,8 @@
+track_list.push(
+    {
+        name: "Love Your Children",
+        artist: "Terrian",
+        image: "Love%20Your%20Children.png",
+        path: "Love%20Your%20Children.002.m4a"
+    }
+);
