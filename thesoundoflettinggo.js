3acc62415c15592e7baa13c020b7c66255bcbe0f
Ben0096 <43043312+Ben0096@users.noreply.github.com>
2023-06-28 02:39:38 -0400

    Create thesoundoflettinggo.js

---
+++ thesoundoflettinggo.js
@@ -0,0 +1,8 @@
+track_list.push(
+    {
+        name: "The Sound of Letting Go",
+        artist: "All Time Low",
+        image: "TellMeImAlive.png",
+        path: "The Sound of Letting Go.001.m4a"
+    }
+);
